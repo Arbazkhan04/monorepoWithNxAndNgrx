fbf1c695164bf9b1b1f7dfe8b3a08185b728dfe5
Arbaz <arbaz582021@gmail.com>
2023-08-03 16:48:50 +0500

    add

--- apps/e-commerence-using-nx-ngrx/src/app/app.component.ts
+++ apps/e-commerence-using-nx-ngrx/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MwnuItemComponent } from './mwnu-item/mwnu-item.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule,MwnuItemComponent],
   selector: 'e-commerence-using-nx-ngrx-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
---
+++ apps/e-commerence-using-nx-ngrx/src/app/mwnu-item/mwnu-item.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'e-commerence-using-nx-ngrx-mwnu-item',
+  templateUrl: './mwnu-item.component.html',
+  styleUrls: ['./mwnu-item.component.css'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
+  ]
+})
+export class MwnuItemComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+}
